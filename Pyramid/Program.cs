8b85e4158e59b7705a9d89eaa6e35276507c8812
Matej <majtan.matej@gmail.com>
2020-08-24 01:05:04 +0200

    added readme and input examples

--- Pyramid/Program.cs
+++ Pyramid/Program.cs
@@ -129,8 +129,30 @@ namespace Pyramid
 
         static void Main(string[] args)
         {
-            string fileContent = File.ReadAllText("../../../input2.txt");
-            var maxSumPath = FindMaxSumPath(fileContent);
+            // Input options:
+            // const string pyramid = "1\n" +
+            //                  "8 9\n" +
+            //                  "1 5 9\n" +
+            //                  "4 5 2 3";
+            const string pyramid = "215\n" +
+                                   "192 124\n" +
+                                   "117 269 442\n" +
+                                   "218 836 347 235\n" +
+                                   "320 805 522 417 345\n" +
+                                   "229 601 728 835 133 124\n" +
+                                   "248 202 277 433 207 263 257\n" +
+                                   "359 464 504 528 516 716 871 182\n" +
+                                   "461 441 426 656 863 560 380 171 923\n" +
+                                   "381 348 573 533 448 632 387 176 975 449\n" +
+                                   "223 711 445 645 245 543 931 532 937 541 444\n" +
+                                   "330 131 333 928 376 733 017 778 839 168 197 197\n" +
+                                   "131 171 522 137 217 224 291 413 528 520 227 229 928\n" +
+                                   "223 626 034 683 839 052 627 310 713 999 629 817 410 121\n" +
+                                   "924 622 911 233 325 139 721 218 253 223 107 233 230 124 233"; 
+            // const string pyramid = File.ReadAllText("../../../input.txt");
+            // const string pyramid = File.ReadAllText("../../../input2.txt");
+
+            var maxSumPath = FindMaxSumPath(pyramid);
             
             Console.WriteLine($"Path with the largest sum of {maxSumPath.Sum()} in the pyramid is:");
             foreach (var num in maxSumPath)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Pyramid Challenge
-Pyramid Challenge
+### How to use
+In the `Main` method of the `Program` class, there are four possible ways to load the pyramid string into the program. First two represent the given pyramids as string constants, that are coded directly in the source code. The other two are loading two files, that are also included in the project, containing these two pyramids. This allows for easy loading of several different pyramids dynamically as they can be stored outside of the source code.
+
+### Decisions along the development
+The data structure of the graph was considered in form of objects, however this either complicates the graph creating process considerably or in other way causes much larger space complexity. In the end, an adjacency list has proven to be simpler approach when loading while it didn't add unnecesary space complexity.
+
+### Testing
+The all functions of the program are tested using unit tests which are all inside the `Tests` project of the solution. These tests were written in order to act as regression tests.
